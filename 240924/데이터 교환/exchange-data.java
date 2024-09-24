16f33502caa16909efb3214634879a3f25863f97
NEWSEASON <154862814+CJW5361@users.noreply.github.com>
2024-09-24 23:14:31 +0900

    Title: 데이터 교환, Time: 85ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240924/데이터 교환/exchange-data.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        int a=5,b=6,c=7;
+        int tmp=a;
+        int tmp2=b;
+        a=c;
+        b=tmp;
+        c=tmp2;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        
+    }
+}
